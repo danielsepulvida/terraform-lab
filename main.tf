5ce2f20b0220a2289bcadd0a88914812b7a6ba68
danielsepulvida <danielsepulvida@homelab-network.io>
2025-02-26 11:19:58 -0300

    Deploy Web Server

--- main.tf
+++ main.tf
@@ -1,12 +1,60 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-    ami = "ami-053a45fff0a704a47"
-    instance_type = "t2.micro"
+  ami                    = "ami-04b4f1a9cf54c11d0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
-    tags = {
-        Name = "terraform-example"
-    }
+  user_data                   = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "terraform-example"
+  }
 }
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+/*
+resource "aws_launch_template" "example" {
+
+  image_id               = "ami-04b4f1a9cf54c11d0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_template.example.name
+  min_size             = 2
+  max_size             = 10
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+*/
